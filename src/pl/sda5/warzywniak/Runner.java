378fddb67c95ad6ef0e9f7748a262102182a59dd
miszalemko <lemkomisza@gmail.com>
2017-11-04 14:15:12 +0100

    Dodanie SklepTest

--- src/pl/sda5/warzywniak/Runner.java
+++ src/pl/sda5/warzywniak/Runner.java
@@ -2,6 +2,8 @@ package pl.sda5.warzywniak;
 
 public class Runner {
     public static void main(String[] args) {
-
+        for(String s : args) {
+            System.out.println(s);
+        }
     }
 }
